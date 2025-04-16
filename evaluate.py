1bb03f445d499fe39a57f8329b06202e0a60f53b
anodicpassion <pratikpawarkarad@gmail.com>
2025-04-16 22:02:54 +0530

    function upgraded to new name

--- evaluate.py
+++ evaluate.py
@@ -3,15 +3,15 @@
 import tensorflow as tf
 from sklearn.metrics import classification_report, confusion_matrix
 import numpy as np
-from utils.preprocessing import load_data
+from utils.preprocessing import load_dataset
 from utils.config import *
 
 model = tf.keras.models.load_model(MODEL_PATH)
-_, _, x_test, y_test = load_data()
+_, x_val, _, y_val = load_dataset()
 
-y_pred = model.predict(x_test)
+y_pred = model.predict(x_val)
 y_pred_classes = np.argmax(y_pred, axis=1)
-y_true = np.argmax(y_test, axis=1)
+y_true = np.argmax(y_val, axis=1)
 
 print("Classification Report:\n")
 print(classification_report(y_true, y_pred_classes))
